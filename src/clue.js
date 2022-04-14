6d0582122fc839769992a6e9466e6b491e08e010
Samy Ali <samy.alisanchez91@gmail.com>
2022-04-14 11:39:21 +0200

    clue done

--- src/clue.js
+++ src/clue.js
@@ -11,7 +11,7 @@ const suspectsArray = [
     description: 'He has a lot of connections',
     image:
       'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
-    color: 'green'
+    color: 'green',
   },
   {
     firstName: 'Doctor',
@@ -20,16 +20,25 @@ const suspectsArray = [
     age: 26,
     description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
     image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
-    color: 'white'
+    color: 'white',
   },
   {
+    firstName: 'Victor',
+    lastName: 'Plum',
+    occupation: 'Designer',
+    age: 22,
+    description: 'Billionaire video game designer',
+    image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
+    color: 'purple',
+},
+{
     firstName: 'Kasandra',
     lastName: 'Scarlet',
     occupation: 'Actor',
     age: 31,
     description: 'She is an A-list movie star with a dark past',
     image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
-    color: 'red'
+    color: 'red',
   },
   {
     firstName: 'Eleanor',
@@ -39,7 +48,7 @@ const suspectsArray = [
     description:
       'She is from a wealthy family and uses her status and money to earn popularity',
     image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
-    color: 'blue'
+    color: 'blue',
   },
   {
     firstName: 'Jack',
@@ -50,29 +59,30 @@ const suspectsArray = [
       'He is a former football player who tries to get by on his former glory',
     image:
       'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
-    color: 'yellow'
+    color: 'yellow',
   }
 ];
+
 
 // Rooms Array
 
 const roomsArray = [
-  {
-    name: 'Dining Room',
-    name: 'Conservatory',
-    name: 'Kitchen',
-    name: 'Study',
-    name: 'Library',
-    name: 'Billiard Room',
-    name: 'Lounge',
-    name: 'Ballroom',
-    name: 'Hall',
-    name: 'Spa',
-    name: 'Living Room',
-    name: 'Observatory',
-    name: 'Theater',
-    name: 'Guest House',
-    name: 'Patio'
+  
+  {name: 'Dining Room'},
+  {name: 'Conservatory'},
+  {name: 'Kitchen'},
+  {name: 'Study'},
+  {name: 'Library'},
+  {name: 'Billiard Room'},
+   {name: 'Lounge'},
+   {name: 'Ballroom'},
+    {name: 'Hall'},
+    {name: 'Spa'},
+    {name: 'Living Room'},
+    {name: 'Observatory'},
+    {name: 'Theater'},
+    {name: 'Guest House'},
+    {name: 'Patio'
   }
 ];
 
@@ -121,6 +131,7 @@ const weaponsArray = [
 function selectRandom(arr) {
   const randomElement = arr[Math.floor(Math.random() * arr.length)];
   return randomElement;
+  // return arr[Math.floor(Math.random() * arr.length)];
 }
 
 function pickMystery() {
@@ -136,9 +147,10 @@ console.log(pickMystery())
 // ITERATION 3
 
 function revealMystery(obj) {
-
+  return `${obj.suspect.firstName} ${obj.suspect.lastName} killed Mr. Boddy using the ${obj.weapon.name} in the ${obj.room.name}!`
 }
 
+console.log(revealMystery(pickMystery()))
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
